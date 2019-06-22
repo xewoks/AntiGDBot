bafed00022827ac6d68b8034f228570e8ebaf07d
c314 <47485054+camden314@users.noreply.github.com>
2019-06-22 10:38:17 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import discord
+import asyncio
+client = discord.Client()
+@client.event
+async def on_ready():
+	while 1:
+		for guild in client.guilds:
+			for member in guild.members:
+				for x in member.activities:
+					if type(x) == discord.Game:
+						print(x)
+						if str(x) == "Fortnite":
+							a = await client.get_user_info(member.id)
+							await a.send(f'You have been banned from `{guild}`\nReason: playing fortnite')
+							await guild.ban(member)
+		await asyncio.sleep(5)
+client.run("TOKEN_HHERE")
